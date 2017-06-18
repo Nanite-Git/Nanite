20703b792952880e21cda6454f90154e79c06112
Nanite-Holg <holgscher@gmail.com>
2017-06-18 12:06:48 +0200

    modify FeatureSort.m

--- Octave/calculate_NN/FeatureSort.m
+++ Octave/calculate_NN/FeatureSort.m
@@ -1,26 +1,27 @@
 %---------------------Reinigen------------------------%
-clear all;
+%clear all;
 clc;
 
-clear X, i;
+%clear X, i;
 %------------------------HistoryData einlesen----------------%
-Data = csvread('HistoryData.csv');
+%Data = csvread('HistoryData.csv');
 %------------------------Konstanten definieren---------------%
 anzfeat = 60;                        %Anzahl Punkte pro Feature
-size_training = size(Data,1)/anz_pp_feature;     %Anzahl Trainingsexamples
+size_training = size(Data,1)/anzfeat;     %Anzahl Trainingsexamples
 diff_values = 6;                            %Anzahl verschiedener Informationsquellen (z.B.: Open, Close, High, Low)
 max_diff = 0;     % Maximale Differenz zwischen zwei Open_Punkten
 %-------------------Speicherallokierung und Initialisierung---------------%
-X = zeros(size(Data,1)-anzfeat,anzfeat*(diff_values-1)+1);
-arr_y  = zeros(size(X,1));        % Differenz zwischen aktuellem Punkt und zukünftigem Punkte
-y = zeros(size(X,1));             % arr_y eingeteilt bzw. klassifiziert in x-Bereiche
+%X = zeros(size(Data,1)-anzfeat,anzfeat*(diff_values-1)+1);
+arr_y  = zeros(size(X,1),1);        % Differenz zwischen aktuellem Punkt und zukünftigem Punkte
+y = zeros(size(X,1),1);             % arr_y eingeteilt bzw. klassifiziert in x-Bereiche
 %-------------------Berechnung x-----------------------%
 for i=0:((size(Data,1)-1)-anzfeat)-1    %erstes -1 da von 0 iteriert, zweites -1 da die Calc für arr_y sonst übereläuft
     
-  X(i+1,:) = [Data(i+anzfeat,1),reshape(Data((i+1):(anzfeat+i),2:diff_values),1,300)];
+  %X(i+1,:) = [Data(i+anzfeat,1),reshape(Data((i+1):(anzfeat+i),2:diff_values),1,300)];
   
-  arr_y(i) = Data(2,i+anzfeat)-Data(2,i+anzfeat+1)
-  
+  if((i+anzfeat+1)<=size(X,1))
+    arr_y(i+1) = Data(i+anzfeat,2)-Data(i+anzfeat+1,2);
+  endif
 %  if mod(i,50000)==0
 %    fprintf('%i',i)
 %    pause;
@@ -30,8 +31,11 @@ endfor
 %--------------------Berechnung y----------------------%
 max_diff = max(abs(arr_y));
 
-for i = -(max_diff/5):(max_diff/5)
-y(arr_y == 0) = 
+for i=0:(max_diff/5):2*max_diff
+
+%y(arr_y == 0) = 
+
+endfor
 
 %x_new = zeros((length(x)/10)-1,10);
 %for i=1:(length(x)/10)-1
